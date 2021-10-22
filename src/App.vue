84d9fc081c725c7296571c514b1621dc3ebcf640
Erin Rivas <erin@atomicbear.com>
2021-10-21 23:04:24 -0500

    Everything working, just need to document and pretty things up

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,127 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <!-- <NavBar /> -->
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+    <v-container>
+      <h2>{{ output }}</h2>
+      <v-text-field v-model="input">test</v-text-field>
+      <div>
+        <v-btn @click="fiboIsIt">Is it a Fibonacci Number?</v-btn>
+        <v-btn @click="fiboPosFind"
+          >What is the position in the sequence?</v-btn
+        >
+        <v-btn @click="fiboFor">What is the number at the position?</v-btn>
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    </v-container>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+// import NavBar from "@/components/NavBar"
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    // NavBar,
   },
 
   data: () => ({
-    //
+    Hash: { max: 0, 0: 0, 1: 1 },
+    input: "",
+    output: "",
   }),
+  methods: {
+    async fiboFor(index, trigger) {
+      const hash = this.Hash || { max: 1, 0: 0, 1: 1 };
+      const safePosition = Number(index) || Number(this.input);
+      if (this.higherTruth(safePosition)) {
+        const working = Math.max(0, safePosition);
+        if (working <= hash.max) {
+          if (trigger) {
+            return hash[working];
+          } else {
+            this.output = hash[working];
+          }
+        } else {
+          for (let i = hash.max; i <= working; i++) {
+            if (i <= 1) {
+              continue;
+            }
+            hash[i] = hash[i - 1] + hash[i - 2];
+            this.Hash[i] = hash[i];
+          }
+          hash.max = working;
+          this.Hash.max = working;
+          if (trigger) {
+            return hash[working];
+          } else {
+            this.output = hash[working];
+          }
+        }
+      } else {
+        this.output = `${this.input} is not a valid submission`;
+        return;
+      }
+    },
+    fiboIsIt() {
+      const safeValue = Number(this.input);
+      if (this.higherTruth(safeValue)) {
+        const coreValue = 5 * safeValue * safeValue;
+        if (this.fiboMath(coreValue)) {
+          this.output = `${safeValue} is a Fibonacci number`;
+          return;
+        } else {
+          this.output = `${safeValue} is not a Fibonacci number`;
+          return;
+        }
+      } else {
+        this.output = `${this.input} is not a valid submission`;
+        return;
+      }
+    },
+    async fiboPosFind() {
+      const hash = this.Hash || { max: 1, 0: 0, 1: 1 };
+      const safeToFind = Number(this.input);
+
+      if (
+        this.higherTruth(safeToFind) &&
+        this.fiboMath(5 * safeToFind * safeToFind)
+      ) {
+        if (safeToFind === 1) {
+          this.output = "Your number is 1, which is both position 1 and 2";
+          return;
+        } else if (safeToFind === hash[hash.max]) {
+          this.output = `Your number is position : ${hash.max}`;
+          return;
+        } else {
+          let i = -1;
+          let curr = 0;
+          while (curr !== safeToFind) {
+            i++;
+            curr = await this.fiboFor(i, true);
+          }
+          this.output = `Your number is position : ${i}`;
+          return;
+        }
+      } else if (this.higherTruth(safeToFind)) {
+        this.output = `${safeToFind} is not a Fibonacci number`;
+        return;
+      } else {
+        this.output = `${this.input} is not a valid submission`;
+        return;
+      }
+    },
+
+    fiboMath(value) {
+      return Math.sqrt(value - 4) % 1 === 0 || Math.sqrt(value + 4) % 1 === 0;
+    },
+    higherTruth(value) {
+      return value === 0 || !!value;
+    },
+  },
 };
 </script>
+
+<style scoped>
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-app-bar app color="primary" dark>
+
+    <v-spacer></v-spacer>
+
+    <v-menu open-on-hover bottom offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn text v-bind="attrs" v-on="on">Learn More</v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item v-for="(resource, index) in learn" :key="index" >
+          <v-btn text @click="navTo(resource.url)">{{ resource.label }}</v-btn>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+
+  data: () => ({
+    learn: [
+      {
+        label: "Wikipedia",
+        url: "https://en.wikipedia.org/wiki/Fibonacci_number",
+      },
+      {
+        label: "Math Is Fun",
+        url: "https://www.mathsisfun.com/numbers/fibonacci-sequence.html",
+      },
+    ],
+  }),
+  methods: {
+      navTo(url) {
+          window.open(url, "_blank")
+      }
+  }
+};
+</script>
