9e29b4b30e676a9ec7bcfc689dc3dd6f55d66b5e
Erin Rivas <erin@atomicbear.com>
2021-10-21 19:44:00 -0500

    Time trials successful, moving to next phase

--- index.js
+++ index.js
@@ -6,13 +6,22 @@ const debug = true
 const Hash = {max: 0, 0: 0, 1: 1}
 
 // Testing
-console.log(fiboWhile(1))
-console.log(fiboWhile(2))
-console.log(fiboWhile(3))
-console.log(fiboWhile(6))
+// console.log(fiboWhile(1))
+// console.log(fiboWhile(2))
+// console.log(fiboWhile(3))
+// console.log(fiboWhile(6))
+
+// fiboStopwatch(10)
+
+async function test() {
+    await fiboStopwatch(100)
+    await fiboStopwatch(101)
+}
+test()
+
 
 // Functions
-function fiboWhile(position) {
+async function fiboWhile(position) {
     const hash = Hash || {max: 1, 0: 0, 1: 1}
     const safePosition = Number(position)
     if (safePosition) {
@@ -24,9 +33,12 @@ function fiboWhile(position) {
                 if (i <= 1) {
                     continue
                 }
+                if (debug) await new Promise(resolve => setTimeout(resolve, 10))
                 hash[i] = hash[i - 1] + hash[i - 2]
                 Hash[i] = hash[i]
             }
+            hash.max = working
+            Hash.max = working
             return hash[working]
         }
     } else {
@@ -34,28 +46,16 @@ function fiboWhile(position) {
     }
 }
 
-
-function dead() {
-
-
-function fiboIterate(count, hash = {}) {
-    // Credit for inspiring this solution goes to https://medium.com/developers-writing/fibonacci-sequence-algorithm-in-javascript-b253dc7e320e
-    
-    // if (debug) console.debug(hash[count])
-    if (count <= 1) {
-        return 1
-    } else if (hash[count]) {
-        return hash[count]
-    } else {
-        return hash[count] = fiboIterate(count - 1, hash) + fiboIterate(count - 2, hash)
-        // return hash[count] = fiboIterate(count - 1, hash) + fiboIterate(count - 2, hash)
-    }
+async function fiboStopwatch(rounds) {
+    console.log(`Difficulty : ${rounds}`)
+    const start = Date.now()
+    const result = await fiboWhile(rounds)
+    const end = Date.now()
+    console.log(`Result : ${result}`)
+    console.log(`Duration : ${end - start}`)
 }
 
 
-    if (memo[num]) return memo[num];
-  if (num <= 1) return 1;
-
-  return memo[num] = fibonacci(num - 1, memo) + fibonacci(num - 2, memo);
+function dead() {
 
 }
