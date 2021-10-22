4d4da8cc91f436a4d81da486ba09f8958c5966ca
Erin Rivas <erin@atomicbear.com>
2021-10-21 19:21:04 -0500

    Working function

--- index.js
+++ index.js
@@ -2,14 +2,40 @@ const rounds = 11
 const seek = 30010
 const debug = true
 
+// Globals
+const Hash = {max: 0, 0: 0, 1: 1}
 
-// console.log(fiboIterate(rounds))
-console.log(fiboIterate(1))
-console.log(fiboIterate(2))
-console.log(fiboIterate(3))
-console.log(fiboIterate(6))
+// Testing
+console.log(fiboWhile(1))
+console.log(fiboWhile(2))
+console.log(fiboWhile(3))
+console.log(fiboWhile(6))
+
+// Functions
+function fiboWhile(position) {
+    const hash = Hash || {max: 1, 0: 0, 1: 1}
+    const safePosition = Number(position)
+    if (safePosition) {
+        const working = Math.max(0, safePosition)
+        if (working <= hash.max) {
+            return hash[working]
+        } else {
+            for (let i = hash.max; i <= working; i++) {
+                if (i <= 1) {
+                    continue
+                }
+                hash[i] = hash[i - 1] + hash[i - 2]
+                Hash[i] = hash[i]
+            }
+            return hash[working]
+        }
+    } else {
+        return "Input must be a number"
+    }
+}
 
 
+function dead() {
 
 
 function fiboIterate(count, hash = {}) {
@@ -25,8 +51,6 @@ function fiboIterate(count, hash = {}) {
         // return hash[count] = fiboIterate(count - 1, hash) + fiboIterate(count - 2, hash)
     }
 }
-
-function dead() {
 
 
     if (memo[num]) return memo[num];
